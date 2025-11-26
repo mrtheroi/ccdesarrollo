e2c564f8e3b2d6e4ed7aeee853fb7f96d127c15b
cesarvalero <cvalero@grupoecce.com>
2025-11-25 21:38:08 -0600

    imports

--- app/Http/Controllers/Api/V1/FinanzasApiController.php
+++ app/Http/Controllers/Api/V1/FinanzasApiController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Models\EstadoResultadosMensual;
+use App\Models\GastoMensualCategoria;
+use App\Models\VentasClienteMensual;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 
 class FinanzasApiController extends Controller
